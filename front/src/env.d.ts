ac80f56083ca0c7d9507a33a4c8e48de2951c863
jeissonmgz <jeissonmgz@gmail.com>
2024-04-20 20:48:49 -0500

    :lipstick: Add screens

--- front/src/env.d.ts
+++ front/src/env.d.ts
@@ -1,7 +1,9 @@
+/// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
     readonly SUPABASE_URL: string
     readonly SUPABASE_ANON_KEY: string
+    readonly API_URL_CLIENT: string;
   }
   
   interface ImportMeta {
